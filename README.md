473604faeb463f56e8a4e34e4e4f1bc8bc192896
vagrant <vagrant@rails-dev-box>
2014-08-05 21:27:08 +0000

    First commit and README update

---
+++ README.md
@@ -0,0 +1,12 @@
+This is my portfolio and resume site.
+Contains:
+Resume
+Links to Projects
+Contact Info
+LinkedIn
+Facebook
+GitHub
+StackOverflow
+JSFiddle
+CodePen
+<tt>rake doc:app</tt>.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hangfirelabs::Application.config.secret_key_base = '5761e1a9e90939adea3a863858f582964ac766f9c3689571434e30fc465a7fc705be0ca6b4b001b31b2c512e734a688f8f14b2dd8cf84ce5bfaaa38e98e9cd59'
